31e18bc4e242203ca0436becf65cbd9b9e37d04f
is-hart <isaacahar@yahoo.com>
2024-01-26 15:08:47 -0500

    Fixed issue?

--- Game/Levels/ThreevenThrodd.lean
+++ Game/Levels/ThreevenThrodd.lean
@@ -5,9 +5,16 @@ import Game.Levels.ThreevenThrodd.L02_mul_threeven
 import Game.Levels.ThreevenThrodd.L03_mul_throver
 import Game.Levels.ThreevenThrodd.L04_mul_thrunder
 import Game.Levels.ThreevenThrodd.L05_mul_thrunder_throver
+import Game.Levels.ThreevenThrodd.L06_sub_threeven
+import Game.Levels.ThreevenThrodd.L07_sub_throver
+import Game.Levels.ThreevenThrodd.L08_sub_thrunder
+import Game.Levels.ThreevenThrodd.L09_thre_thro_or_thru
+import Game.Levels.ThreevenThrodd.L10_boss
 
 World "ThreevenThrodd"
 Title "Threeven/Throver/Thrunder World"
 
 Introduction "
-Wooohooo time for three time"
+It's time for Threeven world! Threeven is like even but with 3s.
+0, 3, 6, 9, and 12 are all threeven. Similarly, 1, 4, 7, and 10 are throver
+and 2, 5, 8, and 11 are thrunder. Have fun!"
